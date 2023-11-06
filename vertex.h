e19cfe706d28317716e9b938d6c605d366d504ff
Dat Nguyen <nguyenxuandat1998@gmail.com>
2023-11-06 12:59:11 +0700

    remove vertex_color 3
    remove vertex_color from vertex.h

--- vertex.h
+++ vertex.h
@@ -7,12 +7,11 @@
 struct Vertex {
     glm::vec3 pos;
     glm::vec3 normal;
-    glm::vec4 color;
     glm::vec2 texCoord;
 
-    Vertex(glm::vec3 p, glm::vec3 n, glm::vec4 c, glm::vec2 uv);
+    Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 uv);
 
     static VkVertexInputBindingDescription getBindingDescription();
 
-    static std::array<VkVertexInputAttributeDescription, 4> getAttributeDescriptions();
+    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
 };
