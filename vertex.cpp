b1feb1008ba59622780bce2fbb392a8f2548e06f
Dat Nguyen <nguyenxuandat1998@gmail.com>
2023-11-26 10:56:02 +0700

    create VertexWithTangent class
    just like Vertex, but add tangent vector

--- vertex.cpp
+++ vertex.cpp
@@ -35,3 +35,45 @@ std::array<VkVertexInputAttributeDescription, 3> Vertex::getAttributeDescription
 
     return attributeDescriptions;
 }
+
+VertexWithTangent::VertexWithTangent(glm::vec3 p, glm::vec3 n, glm::vec3 t, glm::vec2 uv) {
+    pos = p;
+    normal = n;
+    tangent = t;
+    texCoord = uv;
+}
+
+VkVertexInputBindingDescription VertexWithTangent::getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(VertexWithTangent);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return bindingDescription;
+}
+
+std::array<VkVertexInputAttributeDescription, 4> VertexWithTangent::getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
+
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(VertexWithTangent, pos);
+
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(VertexWithTangent, normal);
+
+    attributeDescriptions[2].binding = 0;
+    attributeDescriptions[2].location = 2;
+    attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[2].offset = offsetof(VertexWithTangent, tangent);
+
+    attributeDescriptions[3].binding = 0;
+    attributeDescriptions[3].location = 3;
+    attributeDescriptions[3].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[3].offset = offsetof(VertexWithTangent, texCoord);
+
+    return attributeDescriptions;
+}
--- vertex.h
+++ vertex.h
@@ -15,3 +15,20 @@ struct Vertex {
 
     static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
 };
+
+struct VertexWithTangent {
+    /*
+    A vertex with added tangent vector for normal mapping
+    */
+
+    glm::vec3 pos;
+    glm::vec3 normal;
+    glm::vec3 tangent;
+    glm::vec2 texCoord;
+
+    VertexWithTangent(glm::vec3 p, glm::vec3 n, glm::vec3 t, glm::vec2 uv);
+
+    static VkVertexInputBindingDescription getBindingDescription();
+
+    static std::array<VkVertexInputAttributeDescription, 4> getAttributeDescriptions();
+};
