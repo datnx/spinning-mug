7242d4d0108adb465d7afcf89d88a423dce5e11a
Dat Nguyen <nguyenxuandat1998@gmail.com>
2023-11-30 20:22:42 +0700

    adding inheritance for vertex

--- vertex.h
+++ vertex.h
@@ -4,11 +4,19 @@
 #include <vulkan/vulkan.h>
 #include <array>
 
-struct Vertex {
+struct VertexBase {
+    /*
+    A base vertex. Every vertex will have a position, a normal, and texture coordinates.
+    */
     glm::vec3 pos;
     glm::vec3 normal;
     glm::vec2 texCoord;
+};
 
+struct Vertex : public VertexBase {
+    /*
+    Implementing the base Vertex class
+    */
     Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 uv);
 
     static VkVertexInputBindingDescription getBindingDescription();
@@ -16,15 +24,11 @@ struct Vertex {
     static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
 };
 
-struct VertexWithTangent {
+struct VertexWithTangent : public VertexBase {
     /*
     A vertex with added tangent vector for normal mapping
     */
-
-    glm::vec3 pos;
-    glm::vec3 normal;
     glm::vec3 tangent;
-    glm::vec2 texCoord;
 
     VertexWithTangent(glm::vec3 p, glm::vec3 n, glm::vec3 t, glm::vec2 uv);
 
