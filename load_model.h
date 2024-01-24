db6d3b9907af9bad469fe938b011916a2dc546ae
Dat Nguyen <nguyenxuandat1998@gmail.com>
2024-01-24 16:13:26 +0700

    remove old load_model functions

--- load_model.h
+++ load_model.h
@@ -7,14 +7,8 @@
 #include "vertex.h"
 #include "scene.h"
 
-// load meshes and textures from file
-void load_meshes_and_textures(std::vector<Mesh>& meshes, std::vector<Texture>& textures, std::vector<std::string>& debug_nodes, std::string file_path);
-
 // print node structure for debugging
 void print_node_structure(std::string file_path);
-
-// debug load group275
-void load_group275_debug(std::vector<Mesh>& meshes, std::vector<std::string>& debug_nodes, std::string file_path);
 
 // load obj from scratch
 void load_meshes_and_textures_obj(
