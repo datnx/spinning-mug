19f9816906e5b4abb648086b528f6c555329b5cb
Dat Nguyen <nguyenxuandat1998@gmail.com>
2024-02-19 21:52:38 +0700

    allow descriptor set to be free individually

--- main.cpp
+++ main.cpp
@@ -774,6 +774,7 @@ private:
         // prepare for pool creation
         VkDescriptorPoolCreateInfo poolInfo{};
         poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+        poolInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
         poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
         poolInfo.pPoolSizes = poolSizes.data();
         poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT * (scene->meshes.size() + 2 + scene->textures.size()));
