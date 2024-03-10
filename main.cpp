2babb866cc7177f8b8ff24b68cf67a9705481bfc
Dat Nguyen <nguyenxuandat1998@gmail.com>
2024-03-10 23:33:48 +0700

    create bigger descriptor pool
    for normal maps

--- main.cpp
+++ main.cpp
@@ -940,7 +940,8 @@ private:
         // the second type image samplers for texture mapping
         // TODO: maybe we only need as many descriptors and descriptor set as the number of textures.
         poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        poolSizes[1].descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT * (scene->textures.size()) + 1);
+        poolSizes[1].descriptorCount = static_cast<uint32_t>(
+            MAX_FRAMES_IN_FLIGHT * (scene->textures.size() + scene->normal_maps.size()) + 1);
 
         // prepare for pool creation
         VkDescriptorPoolCreateInfo poolInfo{};
@@ -948,8 +949,9 @@ private:
         poolInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
         poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
         poolInfo.pPoolSizes = poolSizes.data();
-        poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT * (scene->meshes.size() +
-            scene->meshes_with_normal_map.size() + 1 + scene->textures.size()) + 1);
+        poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT *
+            (scene->meshes.size() + scene->meshes_with_normal_map.size() +
+            1 + scene->textures.size() + scene->normal_maps.size()) + 1);
 
         // create the pool
         if (vkCreateDescriptorPool(gpu.logical_gpu, &poolInfo, nullptr, &descriptorPool) != VK_SUCCESS) {
