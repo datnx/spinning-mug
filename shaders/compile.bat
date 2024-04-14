50ce41c79c601369cc8c11769de812a75995c0cd
Dat Nguyen <nguyenxuandat1998@gmail.com>
2024-04-14 23:49:12 +0700

    change vulkan version

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,6 +1,6 @@
-C:\VulkanSDK\1.3.246.1\Bin\glslc.exe shader.vert -o vert.spv
-C:\VulkanSDK\1.3.246.1\Bin\glslc.exe shader.frag -o frag.spv
-C:\VulkanSDK\1.3.246.1\Bin\glslc.exe shader_t.vert -o vert_t.spv
-C:\VulkanSDK\1.3.246.1\Bin\glslc.exe normal_mapping.vert -o normal_mapping_vert.spv
-C:\VulkanSDK\1.3.246.1\Bin\glslc.exe normal_mapping.frag -o normal_mapping_frag.spv
+C:\VulkanSDK\1.3.280.0\Bin\glslc.exe shader.vert -o vert.spv
+C:\VulkanSDK\1.3.280.0\Bin\glslc.exe shader.frag -o frag.spv
+C:\VulkanSDK\1.3.280.0\Bin\glslc.exe shader_t.vert -o vert_t.spv
+C:\VulkanSDK\1.3.280.0\Bin\glslc.exe normal_mapping.vert -o normal_mapping_vert.spv
+C:\VulkanSDK\1.3.280.0\Bin\glslc.exe normal_mapping.frag -o normal_mapping_frag.spv
 pause
