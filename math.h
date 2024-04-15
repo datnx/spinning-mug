88ea5e1c1ddf52c7d90357db19f6cbdc2290a790
Dat Nguyen <nguyenxuandat1998@gmail.com>
2024-04-15 16:22:57 +0700

    implement argsort with bubble sort

--- math.h
+++ math.h
@@ -7,3 +7,28 @@ glm::vec3 mul(glm::mat4 M, glm::vec3 v);
 glm::vec3 cross(glm::vec3 x, glm::vec3 y);
 float dot(glm::vec4 x, glm::vec4 y);
 glm::mat4 transpose(glm::mat4 matrix);
+
+template<typename T>
+void argsort(std::vector<T>& array, std::vector<int>& out) {
+	/*
+	Return the indices order that sorts the array in the decending order
+	*/
+
+	// instantiate the index array
+	out.resize(array.size());
+	for (int i = 0; i < out.size(); i++) out[i] = i;
+
+	// bubble sort algorithm
+	for (int i = out.size() - 1; i > 0; i--) {
+		bool swapped = false;
+		for (int j = 0; j < i; j++) {
+			if (array[out[j]] < array[out[j + 1]]) {
+				int temp = out[j];
+				out[j] = out[j + 1];
+				out[j + 1] = temp;
+				swapped = true;
+			}
+		}
+		if (!swapped) break;
+	}
+}
